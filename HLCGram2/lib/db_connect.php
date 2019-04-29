6163ab96df3936cc849b8d5dfcf7d8a590e026d1
Hugo L. Casanova <casanovh@vanier.college>
2019-04-29 12:29:32 -0400

    Minor change

--- HLCGram2/lib/db_connect.php
+++ HLCGram2/lib/db_connect.php
@@ -1,6 +1,6 @@
 <?php
 //Establish connection: host, user, password, database
-$dbi = mysqli_connect("localhost","PIApp","","CommIT2018");
+$dbi = mysqli_connect("localhost","*****","*****","CommIT2018");
 if ($mysqli->connect_error) {
     die('Connect Error (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
 }
